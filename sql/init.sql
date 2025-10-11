7a160ccfd8746d83b86e7a16a4bf0e8d67ffccba
DantasDaniel-EngDados <contato.dantas.daniel@gmail.com>
2025-10-11 02:52:37 -0300

    Projeto v-3.0.0

--- sql/init.sql
+++ sql/init.sql
@@ -1,6 +1,5 @@
 CREATE TABLE produtos (
-  produtoid SERIAL PRIMARY KEY,
-  nomeproduto VARCHAR(100) NOT NULL,
+  nomeproduto VARCHAR(100) PRIMARY KEY NOT NULL,
   categoria VARCHAR(50) NOT NULL,
   preco NUMERIC(10,2) NOT NULL
 );
